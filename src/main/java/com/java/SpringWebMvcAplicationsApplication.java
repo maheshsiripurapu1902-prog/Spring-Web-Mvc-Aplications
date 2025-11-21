fb01131667de73a8630d4ff34317d7789c7fae18
zayn <siripurapumahesh7670@gmail.com>
2025-11-21 11:41:15 +0530

    spring mvc creating

---
+++ src/main/java/com/java/SpringWebMvcAplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcAplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcAplicationsApplication.class, args);
+	}
+
+}
